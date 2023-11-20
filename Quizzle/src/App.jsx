709543191d65ce4b86ecbb95c6c01e5648cf6ba1
Plamen Milanov <plamenpk@gmail.com>
2023-11-21 00:29:04 +0100

    add userInitialState

--- Quizzle/src/App.jsx
+++ Quizzle/src/App.jsx
@@ -1,13 +1,50 @@
 import Navbar from "./Components/NavBar/NavBar";
 import Sidebar from "./Components/SideBar/SideBar";
+import { useState, useEffect } from "react";
 import AppRouter from "./components/AppRouter/AppRouter"
+import { auth } from './config/firebase-config';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getUserData } from './services/users.services';
+import { AuthContext } from "./context/authContext";
 
 
 const App = () => {
 
+  const [user] = useAuthState(auth);
+  const [appState, setAppState] = useState({
+    user,
+    userData: false,
+  })
+
+  if (appState.user !== user) {
+    setAppState({ user });
+  }
+
+  useEffect(() => {
+    if (user === null) {
+      return;
+    }
+
+    getUserData(user.uid)
+      .then(snapshot => {
+        if (!snapshot.exists()) {
+          throw new Error('User data not found');
+        }
+
+        setAppState({
+          ...appState,
+          userData: Object.keys(snapshot.val())[0]
+          // Object.keys(snapshot.val())[0] returns the first key of the object
+          // Object.keys(snapshot.val()) returns an array of the keys of the object
+          // snapshot.val() returns the value of the object
+          // Google this part when you have lot's of user data Object.keys(snapshot.val())[0] 
+        })
+      })
+  })
+  // console.log(user)
 
   return (
-    <>
+    <AuthContext.Provider value={{ ...appState, setUser: setAppState }}>
       <Navbar />
       <div className="flex">
         <Sidebar />
@@ -15,7 +52,7 @@ const App = () => {
           <AppRouter />
         </div>
       </div>
-    </>
+      </AuthContext.Provider>
   );
 };
 
