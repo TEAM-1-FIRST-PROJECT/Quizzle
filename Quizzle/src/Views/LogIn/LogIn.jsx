84f59fae4cdd14e8e7d24dcb9fe2bf8f60b10c75
Plamen Milanov <plamenpk@gmail.com>
2023-11-21 14:47:25 +0100

    Add LogIn route

---
+++ Quizzle/src/Views/LogIn/LogIn.jsx
@@ -0,0 +1,11 @@
+
+
+const LogIn = () => {
+  return (
+    <div>
+      logIn
+    </div>
+  )
+}
+
+export default LogIn
--- Quizzle/src/components/AppRouter/AppRouter.jsx
+++ Quizzle/src/components/AppRouter/AppRouter.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import RegisterForm from "../../views/Register/Register"
 import Dashboard from '../Dashboard/Dashboard';
+import LogIn from './../../views/LogIn/LogIn';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/register" element={<RegisterForm />} />
       <Route path="/" element={<Dashboard />} />
+      <Route path="/logIn" element={<LogIn />} />
+      
     </Routes>
   )
 }
