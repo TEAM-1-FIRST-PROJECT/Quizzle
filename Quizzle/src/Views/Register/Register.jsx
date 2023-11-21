ff16685be07a2e861fc0b8a8a39cdb58d310855c
lyubaintech <lyubaboyadzhieva@gmail.com>
2023-11-21 19:11:57 +0200

    Refactor Register form design

--- Quizzle/src/Views/Register/Register.jsx
+++ Quizzle/src/Views/Register/Register.jsx
@@ -104,96 +104,87 @@ const RegisterForm = () => {
   };
   return (
     <>
-    <div className="inset-0 bg-hero-pattern bg-cover">
-      <div className="hidden sm:block mt-20 justify-center">
-      <div className="">
-        <form className="w-[450px] mx-auto shadow-xl hover:shadow-violet-400 bg-gray-400 p-8 rounded-lg">
-          <h2 className="text-4x1 text-3xl text-black font-bold text-center">
-            SIGN UP
-          </h2>
-          <div className="flex flex-col text-black py-2">
-            <label>First Name</label>
-            <input
-              className="rounded-lg  mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
-              type="text"
-              value={form.firstName}
-              onChange={updateForm("firstName")}
-            />
-          </div>
-          <div className="flex flex-col text-black py-2">
-            <label>Last Name</label>
-            <input
-              className="rounded-lg  mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
-              type="text"
-              value={form.lastName}
-              onChange={updateForm("lastName")}
-            />
-          </div>
-          <div className="flex flex-col text-black py-2">
-            <label>Username</label>
-            <input
-              className="rounded-lg mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
-              type="text"
-              value={form.username}
-              onChange={updateForm("username")}
-            />
-          </div>
-          <div className="flex flex-col text-black py-2">
-            <label>Email</label>
-            <input
-              className="rounded-lg mt-2 p-2 placeholder-slate-400
+      <div className="h-screen bg-hero-pattern bg-cover flex items-center justify-center">
+        <div className="hidden sm:block mt-20 justify-center">
+          <div className="">
+            <form className="w-[450px] mx-auto shadow-xl hover:shadow-violet-400 bg-indigo-300 p-8 rounded-lg">
+              <h2 className="text-4x1 text-3xl text-black font-bold text-center">
+                Register
+              </h2>
+              <div className="flex flex-col text-black py-2">
+                <label>First Name</label>
+                <input
+                  className="rounded-lg  mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
+                  type="text"
+                  value={form.firstName}
+                  onChange={updateForm("firstName")}
+                />
+              </div>
+              <div className="flex flex-col text-black py-2">
+                <label>Last Name</label>
+                <input
+                  className="rounded-lg  mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
+                  type="text"
+                  value={form.lastName}
+                  onChange={updateForm("lastName")}
+                />
+              </div>
+              <div className="flex flex-col text-black py-2">
+                <label>Username</label>
+                <input
+                  className="rounded-lg mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
+                  type="text"
+                  value={form.username}
+                  onChange={updateForm("username")}
+                />
+              </div>
+              <div className="flex flex-col text-black py-2">
+                <label>Email</label>
+                <input
+                  className="rounded-lg mt-2 p-2 placeholder-slate-400
       focus:outline-none 
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
       invalid:border-pink-500 invalid:text-pink-600
       focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-              type="email"
-              value={form.email}
-              onChange={updateForm("email")}
-            />
+                  type="email"
+                  value={form.email}
+                  onChange={updateForm("email")}
+                />
+              </div>
+              <div className="flex flex-col text-black py-2">
+                <label>Password</label>
+                <input
+                  className="rounded-lg bg-white mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
+                  type="password"
+                  value={form.password}
+                  onChange={updateForm("password")}
+                />
+              </div>
+              <div className=" flex justify-center mt-1">
+                <label>I am an educator</label>
+                <input
+                  className="ml-2"
+                  type="checkbox"
+                  value={form.password}
+                  onChange={updateForm("password")}
+                />
+              </div>
+              <button
+                className="w-full my-5 py-2 bg-violet-400 shadow-xl hover:shadow-violet-600 text-black font-semibold rounded-lg"
+                onClick={handleRegisterUser}
+                type="button"
+              >
+                SIGN UP
+              </button>
+              <p className="text-black-500 hover:text-violet-500 py-2 flex justify-center">
+                Already have an account?{" "}
+              </p>
+            </form>
           </div>
-          <div className="flex flex-col text-black py-2">
-            <label>Password</label>
-            <input
-              className="rounded-lg bg-white mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
-              type="password"
-              value={form.password}
-              onChange={updateForm("password")}
-            />
-          </div>
-          <div className="mt-1">
-            <label>Educator</label>
-            <input
-              className="ml-2"
-              type="checkbox"
-              value={form.password}
-              onChange={updateForm("password")}
-            />
-          </div>
-          <div className="mt-2">
-            <label>Student</label>
-            <input
-              className="ml-2"
-              type="checkbox"
-              value={form.password}
-              onChange={updateForm("password")}
-            />
-          </div>
-          <button
-            className="w-full my-5 py-2 bg-violet-400 shadow-xl hover:shadow-violet-600 text-black font-semibold rounded-lg"
-            onClick={handleRegisterUser}
-            type="button"
-          >
-            SIGN UP
-          </button>
-          <p className="text-indigo-500 py-2 flex justify-center">
-            Already have an account?{" "}
-          </p>
-        </form>
-      </div>
-      </div>
+        </div>
       </div>
     </>
-      
+
   );
 };
 
