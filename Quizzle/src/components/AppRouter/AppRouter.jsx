debb0a1b7d4d98530806a83c52d8bf857e912cf7
TodorT <ttodorov95@icloud.com>
2023-11-21 22:25:33 +0200

    Fixed the name of folder Views to views and fixed an import in AppRouter.jsx

--- Quizzle/src/components/AppRouter/AppRouter.jsx
+++ Quizzle/src/components/AppRouter/AppRouter.jsx
@@ -3,7 +3,7 @@ import RegisterForm from "../../views/Register/Register"
 import Dashboard from '../Dashboard/Dashboard';
 import UserProfile from '../../views/UserProfile/UserProfile';
 import Settings from '../../views/Settings/Settings';
-import LogIn from './../../views/LogIn/LogIn';
+import LogIn from '../../views/LogIn/LogIn';
 
 const AppRouter = () => {
   return (
@@ -13,7 +13,6 @@ const AppRouter = () => {
       <Route path="/profile" element={<UserProfile />} />
       <Route path="/settings" element={<Settings />} />
       <Route path="/logIn" element={<LogIn />} />
-      
     </Routes>
   )
 }
