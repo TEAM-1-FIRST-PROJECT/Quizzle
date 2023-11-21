7ac4ca1a76a51a685d4900b0919bd036c0ff1092
lyubaintech <lyubaboyadzhieva@gmail.com>
2023-11-21 20:57:49 +0200

    Solved LogIn conflict

--- Quizzle/src/Views/LogIn/LogIn.jsx
+++ Quizzle/src/Views/LogIn/LogIn.jsx
@@ -40,7 +40,7 @@ const LogIn = () => {
       })
       .then(() => {
         alert("Login successful, redirecting...");
-          navigate("/");
+        navigate("/");
       })
       .catch((err) => {
         alert(err.message);
@@ -49,67 +49,65 @@ const LogIn = () => {
 
   return (
     <>
-    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
-      <div className="hidden sm:block">
-              </div>
-      <div className="bg-gray-300 flex flex-col justify-center">
-        <form className="max-w-[550px] w-full mx-auto shadow-xl hover:shadow-violet-400 bg-gray-400 p-8 px-8 rounded-lg">
-          <h2 className="text-4x1 text-3xl dark:text-white font-bold text-center">
-          Login
-          </h2>
-          <div className="flex flex-col text-black py-2">
-            <label>Email</label>
-            <input
-              className="rounded-lg mt-2 p-2 focus-within:border-blue-500 shadow-sm placeholder-slate-400
+      <div className="h-screen bg-hero-pattern bg-cover flex items-center justify-center">
+        <div className=" flex flex-col justify-center">
+          <form className="w-[450px] mx-auto shadow-xl hover:shadow-violet-400 bg-indigo-300 p-8 rounded-lg">
+            <h2 className="text-4x1 text-3xl dark:text-white font-bold text-center">
+              Login
+            </h2>
+            <div className="flex flex-col text-black py-2">
+              <label>Email</label>
+              <input
+                className="rounded-lg mt-2 p-2 focus-within:border-blue-500 shadow-sm placeholder-slate-400
               focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
               disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
               invalid:border-pink-500 invalid:text-pink-600
               focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-              type="email"
-              name="email"
-              id="email"
-              value={form.email}
-              onChange={updateForm("email")}
-            />
-          </div>
-          <div className="flex flex-col text-black py-2">
-            <label>Password</label>
-            <input
-             className="rounded-lg dark:bg-white mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
-              type="password"
-              name="password"
-              id="password"
-              value={form.password}
-              onChange={updateForm("password")}
-            />
-          </div>
-          <div className="flex justify-between text-black py-2">
-            <p className="flex items-center">
-              <input className="mr-2" type="checkbox" /> Remember Me
-            </p>
-            <Link >Forgot Password</Link>
-          </div>
-          <button
-           className="w-full my-5 py-2 bg-violet-400 shadow-xl hover:shadow-violet-600 dark:text-white font-semibold rounded-lg"
-            type="button"
-            onClick={handleLogin}
-          >
-            Log In
-          </button>
-          <p className="text-indigo-500 py-2 flex justify-center">
-            Don&#39;t have an account?{" "}
-            <Link
-              className="ml-1 dark:text-white hover:animate-pulse mix-blend-color-dodge"
-              to="/Signup"
+                type="email"
+                name="email"
+                id="email"
+                value={form.email}
+                onChange={updateForm("email")}
+              />
+            </div>
+            <div className="flex flex-col text-black py-2">
+              <label>Password</label>
+              <input
+                className="rounded-lg dark:bg-white mt-2 p-2 focus-within:border-blue-500 focus:outline-none"
+                type="password"
+                name="password"
+                id="password"
+                value={form.password}
+                onChange={updateForm("password")}
+              />
+            </div>
+            <div className="flex justify-between text-black py-2">
+              <p className="flex items-center">
+                <input className="mr-2" type="checkbox" /> Remember Me
+              </p>
+              <Link >Forgot Password</Link>
+            </div>
+            <button
+              className="w-full my-5 py-2 bg-violet-400 shadow-xl hover:shadow-violet-600 dark:text-white font-semibold rounded-lg"
+              type="button"
+              onClick={handleLogin}
             >
-              Sign up
-            </Link>
-          </p>
-        </form>
+              Log In
+            </button>
+            <p className="text-indigo-500 py-2 flex justify-center">
+              Don&#39;t have an account?{" "}
+              <Link
+                className="ml-1 dark:text-white hover:animate-pulse mix-blend-color-dodge"
+                to="/Signup"
+              >
+                Sign up
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
-    </div>
-  </>
-  
+    </>
+
   );
 };
 export default LogIn
