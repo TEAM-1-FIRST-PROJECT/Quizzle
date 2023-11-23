defb0ad4690a88032309057007bdc049d06a2676
lyubaintech <lyubaboyadzhieva@gmail.com>
2023-11-23 18:08:12 +0200

    Add SearchBar component to NavBar

--- Quizzle/src/Components/NavBar/NavBar.jsx
+++ Quizzle/src/Components/NavBar/NavBar.jsx
@@ -2,11 +2,13 @@ import { useState, useContext } from "react";
 import { AuthContext } from "../../context/authContext";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.webp";
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const { user, userData } = useContext(AuthContext);
+  const [searchResults, setSearchResults] = useState([]);
 
   return (
     <nav className="bg-gradient-to-r from-violet-400 to-indigo-400">
@@ -48,11 +50,7 @@ function Navbar() {
             </div>
           </div>
           <div className="flex items-center">
-            <input
-              className="rounded p-1.5 border-2 border-b-4 border-t-4 focus:outline-none hover:shadow-inner hover:shoadow-sm"
-              type="text"
-              placeholder="Search..."
-            />
+          <SearchBar />
             {!user && <Link
               to="/login"
               className=" hover:bg-gradient-to-br hover:from-violet-500 hover:to-fuchsia-400 text-white px-3 py-2 rounded-md"
--- Quizzle/src/services/quiz.services.js
+++ Quizzle/src/services/quiz.services.js
@@ -21,3 +21,46 @@ export const addQuiz = (title, contestType, invitedUsers, timeLimit, category, q
 };
 
 export const quizzesRef = ref(database, "quizzes");
+
+const fromQuizDocument = snapshot => {
+    const quizDocument = snapshot.val();
+  
+    return Object.keys(quizDocument).map(key => {
+      const quiz = quizDocument[key];
+  
+      return {
+        ...quiz,
+        id: key,
+        createdOn: new Date(quiz.createdOn),
+        likedBy: quiz.likedBy ? Object.keys(quiz.likedBy) : [],
+      };
+    });
+  }
+export const getAllQuizzes = () => {
+
+    return get(ref(database, 'quizzes'))
+      .then(snapshot => {
+        if (!snapshot.exists()) {
+          return [];
+        }
+  
+        return fromQuizDocument(snapshot);
+      });
+  };
+
+  export const getQuizById = (id) => {
+
+    return get(ref(database, `quizzes/${id}`))
+      .then(result => {
+        if (!result.exists()) {
+          throw new Error(`Quiz with id ${id} does not exist!`);
+        }
+  
+        const quiz = result.val();
+        quiz.id = id;
+        quiz.createdOn = new Date(quiz.createdOn);
+        if (!quiz.likedBy) quiz.likedBy = [];
+  
+        return quiz;
+      });
+  };
