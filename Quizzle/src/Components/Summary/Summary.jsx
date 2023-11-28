ae6038deeabf3e73a20b7c37495835d27fa25186
Plamen Milanov <plamenpk@gmail.com>
2023-11-28 13:02:03 +0100

    Add auto save result

--- Quizzle/src/Components/Summary/Summary.jsx
+++ Quizzle/src/Components/Summary/Summary.jsx
@@ -9,26 +9,21 @@ const Summary = ({ id, score, title, category }) => {
   const { userData } = useContext(AuthContext)
   const navigate = useNavigate();
 
-  const saveResultHandler = () => {
-    updateUserScore(userData.username, id, title, score, category)
-      .then(() => console.log('Quiz result saved successfully'))
-      .catch((e) => console.error(e));
-    navigate("/");
-  }
+  updateUserScore(userData.username, id, title, score, category)
+    .then(() => console.log('Quiz result saved successfully'))
+    .catch((e) => console.error(e));
 
   return (
     <div className=" bg-indigo-300 flex flex-col items-center justify-center h-screen">
-      <button className="max-w-40rem mx-auto my-8 p-8 pt-10 bg-indigo-300 text-gray-800 rounded-lg shadow-md animate-slide-in-from-bottom"
-        onClick={saveResultHandler}>
+      <button className="max-w-40rem mx-auto my-8 p-8 pt-10 bg-indigo-300 text-gray-800 rounded-lg shadow-md animate-slide-in-from-bottom">
         <p className="text-lg">Quiz Completed!</p>
         <p className="text-lg">You score {score}</p>
-        <div className="mb-4"></div>
-        <p className="text-sm">click here to save result</p>
       </button>
       <button className="mt-1 border-2 px-4 py-1" onClick={() => { navigate('/') }}>Next quiz</button>
     </div>
   )
 }
+
 Summary.propTypes = {
   score: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
