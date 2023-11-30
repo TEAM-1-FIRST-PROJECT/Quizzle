7df2d77f94ce59a9dd2583c44d78b5e85b811cb4
lyubaintech <lyubaboyadzhieva@gmail.com>
2023-11-30 23:23:39 +0200

    NOT DONE - Refactor UserProfile component

--- Quizzle/src/Components/UserProfile/UserProfile.jsx
+++ Quizzle/src/Components/UserProfile/UserProfile.jsx
@@ -1,34 +1,11 @@
-import { useState, useEffect, useContext } from 'react';    
-import { getUserData } from '../../services/users.services';
-import { getQuizById } from '../../services/quiz.services';
+import { useContext } from 'react';    
 import SingleCard from '../SingleQuizCard/SingleQuizCard';
 import { AuthContext } from '../../context/authContext';
 
 
 const UserProfile = () => {
     const { userData } = useContext(AuthContext);
-    const username = userData?.username;
-
-
-    const [userQuizzes, setUserQuizzes] = useState([]);
-
-    useEffect(() => {
-        if (username) {
-          getUserData(username)
-            .then(userData => {
-              const assignedQuizzes = userData.assignedQuizzes || [];
-              console.log(assignedQuizzes); // log the assigned quizzes
-      
-              Promise.all(assignedQuizzes.map(quizId => getQuizById(quizId)))
-                .then(quizzes => {
-                  console.log(quizzes); // log the quizzes
-                  setUserQuizzes(quizzes);
-                })
-                .catch(e => console.error(e));
-            })
-            .catch(e => console.error(e));
-        }
-      }, [username]);
+    const userQuizzes =  userData?.assignedQuizzes ? Object.keys(userData?.assignedQuizzes) : 'unlimited'
 
     return (
         <div className="h-screen bg-hero-pattern-2 bg-cover flex flex-col items-center">
