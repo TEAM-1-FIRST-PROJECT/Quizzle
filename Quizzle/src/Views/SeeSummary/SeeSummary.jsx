f881d392854b8b0f6f9ba242170028d3a67a2db7
Plamen Milanov <plamenpk@gmail.com>
2023-12-02 13:35:04 +0100

    Add summary of quiz results

---
+++ Quizzle/src/Views/SeeSummary/SeeSummary.jsx
@@ -0,0 +1,48 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { getQuizById } from "../../services/quiz.services";
+import toast from "react-hot-toast";
+import { AuthContext } from "../../context/authContext";
+
+const SeeSummary = () => {
+
+  const { id } = useParams();
+  const [quiz, setQuiz] = useState(null);
+  const { userData } = useContext(AuthContext)
+  useEffect(() => {
+    getQuizById(id)
+      .then((fetchedQuiz) => {
+        setQuiz(fetchedQuiz);
+      })
+      .catch((error) => {
+        toast.error("Error fetching quiz details:", error);
+        setQuiz(null);
+      });
+  }, [id]);
+
+  console.log(quiz?.question)
+  //console.log(userData?.score[quiz?.title].userAnswers[0].text)
+  return (
+    <>
+      {quiz && <div className=" flex flex-col items-center justify-center h-screen">
+        <button className="max-w-40rem mx-auto my-8 p-8 pt-10 bg-indigo-100 text-gray-800 rounded-lg shadow-md animate-slide-in-from-bottom">
+          <p className="text-lg">{quiz?.title}</p>
+          <p className="text-lg">{quiz.category}</p>
+          {quiz.question.map((quest, i) => (
+            <div key={i}>
+              <div className="border border-solid border-indigo-300 p-4">
+                <p className="block text-lg">{quest.question}</p>
+                <p1 className="block text-left">correct answer:{quest.answers.find(item => item.isCorrect === true)
+                  ? quest.answers.find(item => item.isCorrect === true).text
+                  : null}</p1>
+                <p1 className="block text-left">your answer:    {userData?.score[quiz?.title].userAnswers[i].text}</p1>
+              </div>
+            </div>
+          ))}
+        </button>
+      </div>}
+    </>
+  )
+}
+
+export default SeeSummary
