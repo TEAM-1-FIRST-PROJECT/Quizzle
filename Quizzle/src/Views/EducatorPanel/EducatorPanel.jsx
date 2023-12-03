6771385004ade840314c05c61aeadd2b0275778e
Plamen Milanov <plamenpk@gmail.com>
2023-12-03 10:41:39 +0100

    Refactored style AssignQuiz

--- Quizzle/src/Views/EducatorPanel/EducatorPanel.jsx
+++ Quizzle/src/Views/EducatorPanel/EducatorPanel.jsx
@@ -33,57 +33,57 @@ const EducatorPanel = () => {
     ? quizzes.filter(quiz => quiz.assignedUsers === undefined || finalDate(quiz.assignedUsers) > dateNow)
     : quizzes.filter(quiz => quiz.scoreBoard !== undefined)
 
-    return (
+  return (
     <div className="bg-black min-h-screen p-5">
-    <div className="mt-[180px] ml-[330px] mr-[50px] justify-center items-center border-4 p-10 rounded-lg bg-gradient-to-bl from-indigo-400 to-cyan-400">
-      <h1 className="mb-5 text-3xl text-white">Quizzes</h1>
-      <input
-        type="text"
-        className="border p-2 rounded mb-5"
-        placeholder="Search for quiz ..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button className="mt-2 px-4 py-2 text-sm font-medium text-white bg-cyan-400 rounded-md hover:bg-green-400 float-right transform transition duration-500 ease-in-out hover:scale-105"
-        onClick={() => { setFinishedQuizzes((!finishedQuizzes)) }}>{finishedQuizzes ? 'switch to finished quizzes' : 'switch to opened quizzes'}</button>
-      <table className="table-auto rounded w-full text-center bg-gradient-to-r from-indigo-400 to-cyan-400 text-white">
-        <thead className=" text-lg">
-          <tr>
-            <th className="border px-4 py-2">Create By</th>
-            <th className="border px-4 py-2">Quiz Title</th>
-            <th className="border px-4 py-2">Created On</th>
-            {finishedQuizzes
-            ?<th className="border px-4 py-2">Assigned students </th>
-            :<th className="border px-4 py-2">Students result</th>}
-            <th className="border px-4 py-2">Assign</th>
-          </tr>
-        </thead>
-        <tbody>
-          {openQuizzes.map((quiz) => (
-            <tr key={quiz.id}>
-
-              <td className="border px-4 py-2">{quiz.createdBy}</td>
-              <td className="border px-4 py-2">{quiz.title }</td>
-              <td className="border px-4 py-2">
-                {dateFormat(quiz.createdOn)}
-              </td>
-              <td className="border px-4 py-2">
-                <Link to={`/quiz-scoreboard/${quiz?.id}`}>
-                {finishedQuizzes
-                  ? (quiz.assignedUsers ? Object.keys(quiz.assignedUsers).length : 0)
-                  : (quiz.scoreBoard ? Object.keys(quiz.scoreBoard).length : 0)}</Link>
-              </td>
-              <td className="border px-4 py-2">
-                <a href={`/assign-quiz/${quiz?.id}`}
-                  className=" px-4 py-1 border border-indigo-500 rounded-lg text-center font-medium hover:bg-indigo-500 hover:text-white dark:hover:bg-dark-1 dark:hover:text-white-300"
-                  onClick={() => { history.push(`/assign-quiz/${quiz?.id}`) }}>Assign</a>
-              </td>
+      <div className="mt-[180px] ml-[330px] mr-[50px] justify-center items-center border-4 p-10 rounded-lg bg-gradient-to-bl from-indigo-400 to-cyan-400">
+        <h1 className="mb-5 text-3xl text-white">Quizzes</h1>
+        <input
+          type="text"
+          className="border p-2 rounded mb-5"
+          placeholder="Search for quiz ..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button className="mt-2 px-4 py-2 text-sm font-medium text-white bg-cyan-400 rounded-md hover:bg-green-400 float-right transform transition duration-500 ease-in-out hover:scale-105"
+          onClick={() => { setFinishedQuizzes((!finishedQuizzes)) }}>{finishedQuizzes ? 'switch to finished quizzes' : 'switch to opened quizzes'}</button>
+        <table className="table-auto rounded w-full text-center bg-gradient-to-r from-indigo-400 to-cyan-400 text-white">
+          <thead className=" text-lg">
+            <tr>
+              <th className="border px-4 py-2">Create By</th>
+              <th className="border px-4 py-2">Quiz Title</th>
+              <th className="border px-4 py-2">Created On</th>
+              {finishedQuizzes
+                ? <th className="border px-4 py-2">Assigned students </th>
+                : <th className="border px-4 py-2">Students result</th>}
+              <th className="border px-4 py-2">Assign</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-            </div>
-            </div>
+          </thead>
+          <tbody>
+            {openQuizzes.map((quiz) => (
+              <tr key={quiz.id}>
+
+                <td className="border px-4 py-2">{quiz.createdBy}</td>
+                <td className="border px-4 py-2">{quiz.title}</td>
+                <td className="border px-4 py-2">
+                  {dateFormat(quiz.createdOn)}
+                </td>
+                <td className="border px-4 py-2">
+                  <Link to={`/quiz-scoreboard/${quiz?.id}`}>
+                    {finishedQuizzes
+                      ? (quiz.assignedUsers ? Object.keys(quiz.assignedUsers).length : 0)
+                      : (quiz.scoreBoard ? Object.keys(quiz.scoreBoard).length : 0)}</Link>
+                </td>
+                <td className="border px-4 py-2">
+                  <a href={`/assign-quiz/${quiz?.id}`}
+                    className=" px-4 py-1 border border-indigo-500 rounded-lg text-center font-medium hover:bg-indigo-500 hover:text-white dark:hover:bg-dark-1 dark:hover:text-white-300"
+                    onClick={() => { history.push(`/assign-quiz/${quiz?.id}`) }}>Assign</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
--- Quizzle/src/views/AssignQuiz/AssignQuiz.jsx
+++ Quizzle/src/views/AssignQuiz/AssignQuiz.jsx
@@ -61,7 +61,7 @@ const AssignQuiz = () => {
 
   return (
     <>
-      {users && <section className="bg-white dark:bg-white py-3 sm:py-5">
+      {users && <div className="ml-1"><section className="bg-white dark:bg-white py-3 sm:py-5">
         <div className="px-4 mx-auto max-w-screen-2xl lg:px-12">
           <div className="relative overflow-hidden bg-white shadow-md dark:bg-indigo-300 opacity-80 sm:rounded-lg mb-20">
             <div className="overflow-x-auto">
@@ -79,10 +79,10 @@ const AssignQuiz = () => {
                     <th scope="col" className="px-4 py-3 bg-indigo-500 text-white"></th>
                     <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Points</th>
                     <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Max possible points</th>
-                    <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Rating</th>
-                    <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Likes</th>
+                    {/* <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Rating</th> */}
+                    {/* <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Likes</th> */}
                     <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Questions</th>
-                    <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Assigned quizzes</th>
+                    {/* <th scope="col" className="px-4 py-3 bg-indigo-500 text-white">Assigned quizzes</th> */}
                   </tr>
                 </thead>
                 <tr className="border-b dark:border-indigo-600 hover:bg-indigo-100 dark:hover:bg-indigo-200">
@@ -92,8 +92,7 @@ const AssignQuiz = () => {
                       <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
                     </div>
                   </td>
-                  <td ></td>
-                  <td ></td>
+                  
                   <td ></td>
                   <td ></td>
                   <td ></td>
@@ -133,8 +132,8 @@ const AssignQuiz = () => {
                       </td>
                       <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">{user.score ? totalScore(Object.values(user.score)) : 0}</td>
 
-                      <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">???0.47</td>
-                      <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">100</td>
+                      {/* <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         <div className="flex items-center">
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -148,20 +147,20 @@ const AssignQuiz = () => {
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                           </svg>
-                          <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <svg aria-hidden="false" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                           </svg>
                           <span className="ml-1 text-gray-500 dark:text-gray-400">5.0</span>
                         </div>
-                      </td>
-                      <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      </td> */}
+                      {/* <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         <div className="flex items-center">
                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2 text-gray-400" aria-hidden="true">
                             <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
                           </svg>
                           1.6M
                         </div>
-                      </td>
+                      </td> */}
                       <td className="px-4 py-2 text-yellow-400">
                         {assignedUsers.length > 0
                           ? assignedUsers.includes(user.username)
@@ -175,7 +174,7 @@ const AssignQuiz = () => {
                               ? <button >Resolved</button>
                               : <button onClick={() => assignQuizHandler(user.username)}>Assign</button>}
                       </td>
-                      <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">{33}</td>
+                      {/* <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">{33}</td> */}
                     </tr>
                   </tbody>
                 ))}
@@ -224,7 +223,52 @@ const AssignQuiz = () => {
             </nav>
           </div>
         </div>
-      </section>}
+      </section></div>}
+      <div className="m-20 justify-center items-center border-4 p-10 rounded-lg bg-gradient-to-bl from-indigo-400 to-cyan-400">
+        <h1 className="mb-5 text-3xl text-white">Quiz </h1>
+        <table className="table-auto rounded w-full text-center bg-gradient-to-r from-indigo-400 to-cyan-400 text-white">
+          <thead className=" text-lg"><tr>
+              <td ></td>
+              <td ></td>
+              <td ></td>
+              <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white text-right">
+                open from <input
+                  type="date"
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </td>
+              <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white text-right">
+                closed on <input
+                  type="date"
+                  onChange={(e) => setFinalDate(e.target.value)}
+                />
+              </td>
+            </tr>
+            <tr>
+              <th className="border px-4 py-2">USER</th>
+              <th className="border px-4 py-2">LAST NAME</th>
+              <th className="border px-4 py-2">POINTS</th>
+              <th className="border px-4 py-2">MAX POSSIBLE POINTS</th>
+              <th className="border px-4 py-2">ASSIGN</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+            <tr>
+
+
+              <td className="border px-4 py-2">username</td>
+              <td className="border px-4 py-2">title</td>
+              <td className="border px-4 py-2">score</td>
+              <td className="border px-4 py-2">100</td>
+              <td className="border px-4 py-2">
+
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
