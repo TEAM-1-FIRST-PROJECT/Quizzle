b632c5f5a9e2218b4344495292b1a2c8db7efb78
lyubaintech <lyubaboyadzhieva@gmail.com>
2023-12-05 22:51:52 +0200

    Update SortButton and UserResultsTable styling

--- Quizzle/src/Components/SortButton/SortButton.jsx
+++ Quizzle/src/Components/SortButton/SortButton.jsx
@@ -5,7 +5,7 @@ import { BiSortAlt2 } from "react-icons/bi";
 const SortButton = ({ onClick }) => {
   return (
     <div>
-    <button className="bg-white hover:bg-indigo-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" onClick={onClick}>
+    <button className="bg-white hover:bg-indigo-400 text-gray-800 text-sm font-semibold py-1 px-2 rounded inline-flex items-center" onClick={onClick}>
     <BiSortAlt2 />
     <span>Sort</span>
   </button>
