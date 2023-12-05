4e371f1cf1fa0b613c404b6c6ec2235cf193ad41
lyubaintech <lyubaboyadzhieva@gmail.com>
2023-12-05 16:43:43 +0200

    Update SingleQuizScoreboard component

--- Quizzle/src/Views/SingleQuizScoreboard/SingleQuizScoreboard.jsx
+++ Quizzle/src/Views/SingleQuizScoreboard/SingleQuizScoreboard.jsx
@@ -23,14 +23,15 @@ const SingleQuizScoreBoard = () => {
             <p className="mt-10">Take a look at the results of all participants.</p>
           </div>
           {quiz && <div className="mt-20 justify-center items-center border-4 p-10 rounded-lg bg-gradient-to-bl from-indigo-400">
-            <h1 className="mb-5 text-5xl bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-stone-100">Quiz {quiz.title}</h1>
+            <h1 className="mb-5 text-5xl bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-stone-100">Quiz: {quiz.title}</h1>
             <table className="table-auto rounded w-full text-center text-white">
               <thead className="text-lg bg-indigo-400">
                 <tr className="border border-violet-200 ">
                   <th className="px-4 py-2">User</th>
                   <th className="px-4 py-2">Quiz Title</th>
-                  <th className="px-4 py-2">Score</th>
+                  <th className="px-4 py-2">Your score</th>
                   <th className="px-4 py-2">Max score</th>
+                  <th className="px-4 py-2">Passing score</th>
                 </tr>
               </thead>
               <tbody>
@@ -41,7 +42,8 @@ const SingleQuizScoreBoard = () => {
                       <td className="px-4 py-2">{user.username}</td>
                       <td className="px-4 py-2">{quiz.title}</td>
                       <td className="px-4 py-2">{user.score}</td>
-                      <td className="px-4 py-2">100</td>
+                      <td className="px-4 py-2">{quiz.maxPassingPoints}</td>
+                      <td className="px-4 py-2">{quiz.minPassingPoints}</td>
                     </tr>
                   ))}
               </tbody>
