f134fc2b4212adad2f9f321a2fb30d13e9d07636
Plamen Milanov <plamenpk@gmail.com>
2023-12-12 22:08:29 +0100

    Refactored fetch quizzes in EducatorPanel

--- Quizzle/src/Views/EducatorPanel/EducatorPanel.jsx
+++ Quizzle/src/Views/EducatorPanel/EducatorPanel.jsx
@@ -6,6 +6,7 @@ import { dateFormat } from "../../common/helpers";
 import { dateNow } from "../../common/constants";
 import { Link } from "react-router-dom";
 import { QUIZ_STATUS } from "../../common/constants";
+import { getAllQuizzes } from "../../services/quiz.services";
 //import toast from "react-hot-toast";
 
 const EducatorPanel = () => {
@@ -15,16 +16,15 @@ const EducatorPanel = () => {
   const [finishedQuizzes, setFinishedQuizzes] = useState(true);
 
   useEffect(() => {
-    onValue(quizzesRef, (snapshot) => {
-      const data = snapshot.val()
-      
-      const filteredQuizzes = Object.values(data).filter(
-        (quiz) =>
-          quiz.title.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-      .filter(quiz => quiz.contestType === QUIZ_STATUS.INVITATIONAL);
-      setQuizzes(filteredQuizzes);
-    });
+    getAllQuizzes()
+      .then(snapshot => {
+        const filteredQuizzes = snapshot.filter(
+          (quiz) =>
+            quiz.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+          .filter(quiz => quiz.contestType === QUIZ_STATUS.INVITATIONAL);
+        setQuizzes(filteredQuizzes);
+      })
   }, [searchTerm]);
 
   const finalDate = (ob) => {
@@ -37,12 +37,12 @@ const EducatorPanel = () => {
     : quizzes.filter(quiz => quiz.scoreBoard !== undefined)
 
   return (
-    <div className="h-screen pb-20 overflow-auto p-5">
+    <div className="pb-20 overflow-auto p-5">
       <div className="ml-20 text-center text-3xl animate-fade-in font-bold bg-clip-text text-transparent bg-gradient-to-r from-zinc-800 to-gray-500 dark:bg-gradient-to-r dark:from-zinc-300 dark:to-gray-500">
-      <p className="mt-10">On this page, you can view all the quizzes.</p>
-      <p className="mt-5 ">You have the option to search for a quiz using the search field,</p>
+        <p className="mt-10">On this page, you can view all the quizzes.</p>
+        <p className="mt-5 ">You have the option to search for a quiz using the search field,</p>
         <p className="mt-5"> and you can assign a specific quiz to a particular student.</p>
-        </div>
+      </div>
       <div className="mt-20 justify-center items-center border-4 p-10 rounded-lg bg-gradient-to-br from-indigo-500 to-indigo- dark:bg-gradient-to-br dark:from-zinc-600">
         <h1 className="mb-5 text-5xl bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-zinc-600 dark:text-zinc-300">Quizzes</h1>
         <input
@@ -66,30 +66,30 @@ const EducatorPanel = () => {
             </tr>
           </thead>
           <tbody>
-             {openQuizzes.length > 0 ? (
-            openQuizzes.map((quiz) => (
-              <tr key={quiz.id} className="border dark:bg-gradient-to-br dark:from-zinc-800">
-                <td className=" px-4 py-2">{quiz.title}</td>
-                <td className=" px-4 py-2">
-                  {dateFormat(quiz.createdOn)}
-                </td>
-                <td className="px-4 py-2">
-                  <Link to={`/quiz-scoreboard/${quiz?.id}`}>
-                    {finishedQuizzes
-                      ? (quiz.assignedUsers ? Object.keys(quiz.assignedUsers).length : 0)
-                      : (quiz.scoreBoard ? Object.keys(quiz.scoreBoard).length : 0)}</Link>
-                </td>
-                <td className=" px-4 py-2">
-                  <Link to={`/assign-quiz/${quiz?.id}`}
-                    className=" px-4 py-1  border-indigo-500 bg-indigo-400 dark:bg-indigo-600 dark:hover:bg-indigo-500 rounded text-center font-medium hover:bg-indigo-500 hover:text-white dark:hover:bg-dark-1 dark:hover:text-white-300"
-                  >Assign</Link>
-                </td>
-              </tr>
-            ))
-            ):(
+            {openQuizzes.length > 0 ? (
+              openQuizzes.map((quiz) => (
+                <tr key={quiz.id} className="border dark:bg-gradient-to-br dark:from-zinc-800">
+                  <td className=" px-4 py-2">{quiz.title}</td>
+                  <td className=" px-4 py-2">
+                    {dateFormat(quiz.createdOn)}
+                  </td>
+                  <td className="px-4 py-2">
+                    <Link to={`/quiz-scoreboard/${quiz?.id}`}>
+                      {finishedQuizzes
+                        ? (quiz.assignedUsers ? Object.keys(quiz.assignedUsers).length : 0)
+                        : (quiz.scoreBoard ? Object.keys(quiz.scoreBoard).length : 0)}</Link>
+                  </td>
+                  <td className=" px-4 py-2">
+                    <Link to={`/assign-quiz/${quiz?.id}`}
+                      className=" px-4 py-1  border-indigo-500 bg-indigo-400 dark:bg-indigo-600 dark:hover:bg-indigo-500 rounded text-center font-medium hover:bg-indigo-500 hover:text-white dark:hover:bg-dark-1 dark:hover:text-white-300"
+                    >Assign</Link>
+                  </td>
+                </tr>
+              ))
+            ) : (
               <tr>
-              <td colSpan="4" className="text-center py-4 text-2xl">No results found</td>
-            </tr>
+                <td colSpan="4" className="text-center py-4 text-2xl">No results found</td>
+              </tr>
             )}
           </tbody>
         </table>
